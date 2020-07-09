cd6c9b0ba0a57a414550549eb872d59c38737d24
suzuki-navi <suzuki.navi.dev@gmail.com>
2020-07-09 23:11:01 +0900

    first scripts

---
+++ initialize.sh
@@ -0,0 +1,34 @@
+
+set -Ceu
+
+mkdir -p $HOME/.mydocker2/credentials
+chmod 700 $HOME/.mydocker2/credentials
+
+touch $HOME/.mydocker2/credentials.raw.txt
+touch $HOME/.mydocker2/credentials.raw.txt.1
+chmod 600 $HOME/.mydocker2/credentials.raw.txt
+chmod 600 $HOME/.mydocker2/credentials.raw.txt.1
+
+if [ ! -e $HOME/.mydocker2/credentials.txt ]; then
+    echo -n "Input credential: "
+    read line
+    echo "$line" > $HOME/.mydocker2/credentials.txt
+fi
+
+(
+    cd $HOME/.mydocker2/credentials
+    cat $HOME/.mydocker2/credentials.txt | base64 -d | openssl enc -d -aes256 -pbkdf2 >| $HOME/.mydocker2/credentials.tar.gz
+    tar xzf $HOME/.mydocker2/credentials.tar.gz
+)
+
+(
+    find $HOME/.mydocker2/credentials -type f | LC_ALL=C sort | while read path; do
+        echo $path
+        cat $path
+    done
+) >| $HOME/.mydocker2/credentials.raw.txt
+
+if [ -e $HOME/.mydocker2/credentials/initialize.sh ]; then
+    bash $HOME/.mydocker2/credentials/initialize.sh
+fi
+
---
+++ pack.sh
@@ -0,0 +1,25 @@
+
+set -Ceu
+
+(
+    find $HOME/.mydocker2/credentials -type f | LC_ALL=C sort | while read path; do
+        echo $path
+        cat $path
+    done
+) >| $HOME/.mydocker2/credentials.raw.txt.1
+
+if ! cmp -s $HOME/.mydocker2/credentials.raw.txt $HOME/.mydocker2/credentials.raw.txt.1; then (
+    diff -u $HOME/.mydocker2/credentials.raw.txt $HOME/.mydocker2/credentials.raw.txt.1 || true
+
+    cd $HOME/.mydocker2/credentials
+
+    tar czf $HOME/.mydocker2/credentials.tar.gz .
+    cat $HOME/.mydocker2/credentials.tar.gz | openssl enc -e -aes256 -pbkdf2 | base64 | tr -d '\n' >| $HOME/.mydocker2/credentials.txt
+    echo >> $HOME/.mydocker2/credentials.txt
+
+    cat $HOME/.mydocker2/credentials.txt
+    cp $HOME/.mydocker2/credentials.raw.txt.1 $HOME/.mydocker2/credentials.raw.txt
+) else
+    echo "Credentials no changes"
+fi
+
