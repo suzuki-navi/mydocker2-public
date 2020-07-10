dc9556bfd2db3c4f899ebf1c220715f7d309b2f2
suzuki-navi <suzuki.navi.dev@gmail.com>
2020-07-10 16:09:23 +0900

    modify lib/copy.sh

--- lib/copy.sh
+++ lib/copy.sh
@@ -20,6 +20,9 @@ function copy() {
     fi
 
     if [ -d $fname ]; then
+        if [ ! -e $home_path/$fname -a $direction = from-home ]; then
+            return 0
+        fi
         (
             cd $fname
             mkdir -p $home_path/$fname
@@ -30,10 +33,70 @@ function copy() {
 
     if [ -f $fname ]; then
         if [ $type = overwrite-partial ]; then
+            type=overwrite
+        fi
+
+        if [ $type = overwrite ]; then
             if [ $direction = to-home ]; then
-                cp --preserve=mode,timestamp -vf $fname $home_path/$fname
+                if ! cmp -s $fname $home_path/$fname; then
+                    echo $(pwd)/$fname -\> $home_path/$fname
+                    cp --preserve=mode,timestamp -f $fname $home_path/$fname
+                fi
             elif [ $direction = from-home ]; then
-                cp --preserve=mode,timestamp -vf $home_path/$fname $fname
+                if [ -e $home_path/$fname ] && ! cmp -s $fname $home_path/$fname; then
+                    echo $home_path/$fname -\> $(pwd)/$fname
+                    cp --preserve=mode,timestamp -f $home_path/$fname $fname
+                fi
+            fi
+        elif [ $type = history ]; then
+            if [ $direction = to-home ]; then
+                if ! cmp -s $fname $home_path/$fname; then
+                    echo $(pwd)/$fname -\> $home_path/$fname
+                    (
+                        cat $fname
+                        diff -u $fname $home_path/$fname | grep -a -e '^+' | grep -a -v -e '^+++' | cut -b2-
+                    ) >| $home_path/$fname.merged
+                    mv $home_path/$fname.merged $home_path/$fname
+                fi
+            elif [ $direction = from-home ]; then
+                if [ -e $home_path/$fname ] && ! cmp -s $fname $home_path/$fname; then
+                    echo $home_path/$fname -\> $(pwd)/$fname
+                    (
+                        cat $fname
+                        diff -u $fname $home_path/$fname | grep -a -e '^+' | grep -a -v -e '^+++' | cut -b2-
+                    ) >| $fname.merged
+                    mv $fname.merged $fname
+                fi
+            fi
+        elif [ $type = known-hosts ]; then
+            if [ $direction = to-home ]; then
+                if ! cmp -s $fname $home_path/$fname; then
+                    echo $(pwd)/$fname -\> $home_path/$fname
+                    cat $fname $home_path/$fname | perl -e '
+                        my @arr = ();
+                        while (my $line =<STDIN>) {
+                            unless (grep {$_ eq $line} @arr) {
+                                print $line;
+                                push(@arr, $line);
+                            }
+                        }
+                    ' >| $home_path/$fname.merged
+                    mv $home_path/$fname.merged $home_path/$fname
+                fi
+            elif [ $direction = from-home ]; then
+                if [ -e $home_path/$fname ] && ! cmp -s $fname $home_path/$fname; then
+                    echo $home_path/$fname -\> $(pwd)/$fname
+                    cat $fname $home_path/$fname | perl -e '
+                        my @arr = ();
+                        while (my $line =<STDIN>) {
+                            unless (grep {$_ eq $line} @arr) {
+                                print $line;
+                                push(@arr, $line);
+                            }
+                        }
+                    ' >| $fname.merged
+                    mv $fname.merged $fname
+                fi
             fi
         fi
     fi
